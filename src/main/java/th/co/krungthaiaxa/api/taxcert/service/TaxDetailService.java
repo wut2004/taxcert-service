b0f67566aac36880c0f0ba595cad9850624ef2d8
wut2004 <tomwuts@gmail.com>
2016-07-31 18:54:24 +0700

    initial

---
+++ src/main/java/th/co/krungthaiaxa/api/taxcert/service/TaxDetailService.java
@@ -0,0 +1,8 @@
+package th.co.krungthaiaxa.api.taxcert.service;
+
+/**
+ * Created by ptom_nmay on 7/31/2016 AD.
+ */
+public class TaxDetailService {
+
+}
---
+++ src/main/java/th/co/krungthaiaxa/api/taxcert/service/TaxHeadService.java
@@ -0,0 +1,9 @@
+package th.co.krungthaiaxa.api.taxcert.service;
+
+/**
+ * Created by ptom_nmay on 7/31/2016 AD.
+ */
+public class TaxHeadService {
+
+    //Interact with database
+}
